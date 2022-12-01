a3f3db3ef83830d0c98c24ae056fe647109d24d8
Fernanda Montesano <fernanda.montesano@gmail.com>
2022-12-01 15:05:42 -0300

    Create Test Lead Completed

--- src/main/java/pageConstants/LeadPageConstants.java
+++ src/main/java/pageConstants/LeadPageConstants.java
@@ -1,5 +1,22 @@
 package pageConstants;
 
 public class LeadPageConstants {
+	
+	
+	/*
+	 * Create Account Page elements General
+	 */	
+	public static final String LEAD_FIRSTNAME="//label[text()='First Name']/following-sibling::div/input";
+	public static final String LEAD_LASTNAME ="//label[text()='Last Name']/following-sibling::div/input";
+	public static final String LEAD_PHONE ="//label[text()='Phone']/following-sibling::div/input";
+	public static final String LEAD_COMPANY ="//label[text()='Company']/following-sibling::div/input";
+	public static final String LEAD_SOURCE_FIELD ="//label[text()='Lead Source']/following-sibling::div/lightning-base-combobox";
+	public static final String LEAD_SOURCE_OPTION ="//span[text()='+@SOURCE+']";
+	public static final String LEAD_STATUS_FIELD ="//label[text()='Lead Status']/following-sibling::div//lightning-base-combobox";
+	public static final String LEAD_STATUS_OPTION ="//span[text()='+@STATUS+']";
+	public static final String LEAD_RATING_FIELD ="//label[text()='Rating']/following-sibling::div/lightning-base-combobox";
+	public static final String LEAD_RATING_OPTION ="//span[text()='+@RATING+']";
+
+
 
 }
